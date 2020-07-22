1c1eaf5a27768afd725c3130d89fd244151acfa2
MontagueM <msm19@ic.ac.uk>
2020-07-23 00:15:58 +0100

    Added scale factor that sort of works

--- Good Python Files/map_unpacker.py
+++ Good Python Files/map_unpacker.py
@@ -73,12 +73,12 @@ def unpack_map():
     """
     # Need to add scale
     transform_hex = hex_data[192 * 2:216912 * 2]
-    coords, rotations = get_transform_data(transform_hex)
+    coords, rotations, scales = get_transform_data(transform_hex)
     model_refs_hex = hex_data[216944*2:219076*2]
     model_refs = get_model_refs(model_refs_hex)
     copy_count_hex = hex_data[219104*2:]
     copy_counts = get_copy_counts(copy_count_hex)
-    transforms_array = get_transforms_array(model_refs, copy_counts, coords, rotations)
+    transforms_array = get_transforms_array(model_refs, copy_counts, coords, rotations, scales)
     obj_strings = get_model_obj_strings(transforms_array)
     write_obj_strings(obj_strings)
 
@@ -105,36 +105,51 @@ def get_transform_data(transform_hex):
             else:
                 coords.append(floats)
 
-    # Other option from 3a7
-    # transform_hex = get_hex_data(test_dir + '0369-000003A7.bin')[192 * 2:216912 * 2]
-    # entries_hex = [transform_hex[i:i + 48 * 2] for i in range(0, len(transform_hex), 48 * 2)]
-    # model_entries = []
-    # for entry_hex in entries_hex:
-    #     entry_header = ModelEntry()
-    #     model_entries.append(get_header(entry_hex, entry_header))
-    #
-    # coords1 = []
-    # coords2 = []
-    # for e in entries_hex:
-    #     hexes = [e[:12 * 2], e[16 * 2:28 * 2]]
-    #     for k, h in enumerate(hexes):
-    #         # print(h)
-    #         hex_floats = [h[i:i + 8] for i in range(0, len(h), 8)]
-    #         floats = []
-    #         for hex_float in hex_floats:
-    #             # We don't need to worry about flipping hex as this unpack is:
-    #             # 1. Little Endian 2. DCBA (so reverses the direction)
-    #             float_value = struct.unpack('f', bytes.fromhex(hex_float))[0]
-    #             floats.append(round(float_value, 3))
-    #         # Formatting for x,y,z
-    #         coord = [floats[i:i + 3] for i in range(0, len(floats), 3)][0]
-    #         # print(coord)
-    #         if k == 0:
-    #             coords1.append(coord)
-    #         else:
-    #             coords2.append(coord)
+    # Scale
+    hex_data_3a7 = get_hex_data(test_dir + '0369-000003A7.bin')[48 * 2:]
+    entries_hex = [hex_data_3a7[i:i + 48 * 2] for i in range(0, len(hex_data_3a7), 48 * 2)]
 
-    return coords, rotations
+    coords1 = []
+    coords2 = []
+    for e in entries_hex:
+        hexes = [e[:12 * 2], e[16 * 2:28 * 2]]
+        for k, h in enumerate(hexes):
+            hex_floats = [h[i:i + 8] for i in range(0, len(h), 8)]
+            floats = []
+            for hex_float in hex_floats:
+                # We don't need to worry about flipping hex as this unpack is:
+                # 1. Little Endian 2. DCBA (so reverses the direction)
+                float_value = struct.unpack('f', bytes.fromhex(hex_float))[0]
+                floats.append(float_value)
+            # Formatting for x,y,z
+            if len(floats) == 3:
+                coord = [floats[i:i + 3] for i in range(0, len(floats), 3)][0]
+            else:
+                coord = floats[0]
+            # print(coord)
+            if k == 0:
+                coords1.append(coord)
+            elif k == 1:
+                coords2.append(coord)
+
+    # Getting modifier number from b77
+    hex_data_b77 = hex_data[192*2:216912*2]
+    entries_hex = [hex_data_b77[i:i + 48 * 2] for i in range(0, len(hex_data_b77), 48 * 2)]
+    modifiers = []
+    for e in entries_hex:
+        hex = e[28 * 2:32 * 2]
+        hex_floats = [hex[i:i + 8] for i in range(0, len(hex), 8)]
+        floats = []
+        for hex_float in hex_floats:
+            float_value = struct.unpack('f', bytes.fromhex(hex_float))[0]
+            floats.append(float_value)
+        modifier = floats
+        modifiers.append(modifier)
+    ###
+    sub = np.array(coords2) - np.array(coords1)
+    scale_factors = [[round(x, 4) for x in sub[i] / np.array(modifiers[i])] for i in range(len(sub))]
+
+    return coords, rotations, scale_factors
 
 
 def get_model_refs(model_refs_hex):
@@ -150,21 +165,21 @@ def get_copy_counts(copy_count_hex):
     return [e.Field0 for e in entries]
 
 
-def get_transforms_array(model_refs, copy_counts, coords, rotations):
-    print(coords[-1])
+def get_transforms_array(model_refs, copy_counts, coords, rotations, scales):
+    # print(coords[-1])
     transforms_array = []
     last_index = 0
     for i, model in enumerate(model_refs):
         copies = copy_counts[i]
         transforms = []
-        print(last_index, len(coords))
-        print(copies)
+        # print(last_index, len(coords))
+        # print(copies)
         for copy_id in range(copies):
-            transforms.append([coords[last_index + copy_id], rotations[last_index + copy_id]])
+            transforms.append([coords[last_index + copy_id], rotations[last_index + copy_id], scales[last_index + copy_id]])
         last_index += copies
         transform_array = [model, transforms]
         transforms_array.append(transform_array)
-    print(last_index, len(coords))
+    # print(last_index, len(coords))
     return transforms_array
 
 
@@ -172,8 +187,8 @@ def get_model_obj_strings(transforms_array):
     obj_strings = []
     max_vert_used = 0
     for i, transform_array in enumerate(transforms_array):
-        if i > 0:
-            return obj_strings
+        # if i > 100:
+        #     return obj_strings
         print(f'Getting obj {i+1}/{len(transforms_array)} {transform_array[0]}')
         verts_data, faces_data = model_unpacker.get_verts_faces_data(transform_array[0])
         if not verts_data or not faces_data:
@@ -181,8 +196,10 @@ def get_model_obj_strings(transforms_array):
             continue
         for copy_id, transform in enumerate(transform_array[1]):
             # print(f'{transform_array[0]}_{copy_id}')
-            # s_verts_data = scale_verts(verts_data)
-            sm_verts_data = move_verts(verts_data, transform[0])
+            s_verts_data = scale_verts(verts_data, transform[2])
+            # Just testing
+            # r_verts_data = rotate_verts(verts_data, [1, 0, 0, 1])
+            sm_verts_data = move_verts(s_verts_data, transform[0])
             smr_verts_data = rotate_verts(sm_verts_data, transform[1])
             adjusted_faces_data, max_vert_used = adjust_faces_data(faces_data, max_vert_used)
             obj_str = model_unpacker.get_obj_str(adjusted_faces_data, smr_verts_data)
@@ -209,8 +226,7 @@ def rotate_verts(verts_data, rotation_transform):
     y = rotation_transform[2]
     z = rotation_transform[3]
     r = scipy.spatial.transform.Rotation.from_quat([x, y, z, w])
-    quat_rots = scipy.spatial.transform.Rotation.apply(r, verts_data, inverse=True)
-
+    quat_rots = scipy.spatial.transform.Rotation.apply(r, verts_data, inverse=False)
     return quat_rots
 
 
@@ -222,18 +238,14 @@ def move_verts(verts_data, move_transform):
     return moved_verts
 
 
-def scale_verts(verts_data):
-    verts_data = [(np.array(x)*7.03).tolist() for x in verts_data]
-    return verts_data
+def scale_verts(verts_data, scale_transform):
+    return np.multiply(scale_transform, verts_data)
 
 
 def write_obj_strings(obj_strings):
     with open('unpacked_objects/city_tower_d2_0369.obj', 'w') as f:
         for string in obj_strings:
             f.write(string)
-        # for string in obj_strings:
-        #     for line in string:
-        #         f.write(line)
     print('Written to file.')
 
 
